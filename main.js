581e2963d6765e17251acc6083391bd415e8971b
Alber321 <lsolis2@ucol.mx>
2020-11-07 22:53:30 -0600

    Final

--- main.js
+++ main.js
@@ -101,12 +101,12 @@ class Inventario{
     posi.siguiente = nuevop
     this.size++
   }
-    AgregarFinal(codigo, nombre, desc, cantidad, coste){
-        let nuevop = new Producto(codigo, nombre, desc, cantidad, coste, null)
+    AgregarFinal(item){
+        let nuevop = new Producto(item[0], item[1], item[2], item[3], item[4], item[5], null)
         if(!this.inicio){
             this.inicio = nuevop
         }else{
-            let current = this.head;
+            let current = this.inicio;
             while(current.siguiente){
                 current = current.siguiente;
             };
@@ -121,7 +121,7 @@ btnagregar.addEventListener("click", () => {
     if(Mostrar.Verificar(codigo.value)==false){
         let p = new Array (codigo.value,nombre.value,desc.value,cantidad.value,coste.value,(Number(cantidad.value)* Number(coste.value)))
         Mostrar.AgregarFinal(p)
-        lista.innerHTML = "Codigo: "+p[0]+"Nombre: "+p[1]+"Descripcion: "+p[2]+"Cantidad: "+p[3]+"Coste: "+p[4]+"Total: "+p[5]+"-"+ "</br > Agregado con exito!"
+        lista.innerHTML = "Codigo: "+ p[0] + " Nombre: "+ p[1]+ " Descripcion: "+p[2]+" Cantidad: "+p[3]+" Coste: "+p[4]+" Total: "+p[5]+ "</br > Agregado con exito!"
     }else{
         alert ("Error! Codigo repetido")
     };
@@ -129,7 +129,7 @@ btnagregar.addEventListener("click", () => {
 btnborrar.addEventListener("click", () =>{
     let d = Mostrar.BuscarCode(codigo.value)
     if(d!=null){
-        lista.innerHTML = +"Codigo: "+d.codigo+"Nombre: "+d.nombre+"Descripcion: "+d.desc+"Cantidad: "+d.cantidad+"Coste: "+d.coste+"Total: "+d.total+"-"+"</br > Eliminado con exito"
+        lista.innerHTML = +"Codigo: "+d.codigo+" Nombre: "+d.nombre+" Descripcion: "+d.desc+" Cantidad: "+d.cantidad+" Coste: "+d.coste+" Total: "+d.total+"</br > Eliminado con exito"
         Mostrar.EliminarCode(d)
     }else{
         alert("El producto seleccionado no Existe!")
@@ -138,7 +138,7 @@ btnborrar.addEventListener("click", () =>{
 btnbuscar.addEventListener("click", () =>{
     let busc = Mostrar.BuscarCode(codigo.value)
     if(busc!=null){
-        lista.innerHTML = "Codigo: "+busc.codigo+"Producto: "+ busc.nombre+"Descripcion: "+ busc.desc+"Cantidad: "+busc.cantidad+"Coste: "+busc.coste+"Total: "+busc.total+ "</br > Producto Encontrado!"
+        lista.innerHTML = "Codigo: "+busc.codigo+" Producto: "+ busc.nombre+" Descripcion: "+ busc.desc+" Cantidad: "+busc.cantidad+" Coste: "+busc.coste+" Total: "+busc.total+ "</br > Producto Encontrado!"
     }else{
         alert("El producto No existe. Intente con otro codigo")
     }
@@ -148,15 +148,56 @@ btnmostar.addEventListener("click", () =>{
     let current = null
     for(let i=0; i!=Mostrar.size;i++){
         current = Mostrar.Recorrer(current)
-        lista.innerHTML += "Codigo: "+current.codigo+"Producto: "+ current.nombre+"Descripcion: "+ current.desc+"Cantidad: "+current.cantidad+"Coste: "+current.coste+"Total: "+current.total+ "</br >"
+        lista.innerHTML += "Codigo: "+current.codigo+" Producto: "+ current.nombre+" Descripcion: "+ current.desc+" Cantidad: "+current.cantidad+" Coste: "+current.coste+" Total: "+current.total+ "</br >"
     }
+    lista.innerHTML += "Lista Completa!"
 });
 btnmostrarinv.addEventListener("click", () =>{
     lista.innerHTML = ""
     let current = null
-    let inv = new Array()
+    let inver = new Array()
         for(let i=0; i!=Mostrar.size;i++){
-            lista.innerHTML += "Codigo: "+busc.codigo+"Producto: "+ busc.nombre+"Descripcion: "+ busc.desc+"Cantidad: "+busc.cantidad+"Coste: "+busc.coste+"Total: "+busc.total+ "</br >"
+            current = Mostrar.Recorrer(current)
+            inver[i] = current
+        }
+        for(let i=Mostrar.size-1;i!=-1;i--){
+            lista.innerHTML += "Codigo: "+inver[i].codigo+" Producto: "+ inver[i].nombre+" Descripcion: "+ inver[i].desc+" Cantidad: "+inver[i].cantidad+" Coste: "+inver[i].coste+" Total: "+inver[i].total+ "</br >"
         }
         lista.innerHTML += "Lista Completa!"
+});
+btninsertar.addEventListener("click", () =>{
+    if(Mostrar.Verificar(codigo.value)==true){
+        alert("Este codigo ya existe")
+    }else if(Number(posicion.value)> Mostrar.size){
+        alert("No existen datos en la posición")
+    }else{
+        let pos = Number(posicion.value)
+        let current = null
+        let ins = new Array (codigo.value,nombre.value,desc.value,cantidad.value,coste.value,(Number(cantidad.value)* Number(coste.value)))
+        for(let i=0; i!=pos;i++){
+            current = Mostrar.Recorrer(current);
+            if(i+1==pos){
+                Mostrar.posEspecifico(current,ins)
+            }
+        }
+        lista.innerHTML = ins[0]+" "+ins[1]+" "+ins[2]+" "+ins[3]+" "+ins[4]+" "+ins[5]+ "</br > Insertado con exito"
+    }
+});
+btnagregarprimero.addEventListener("click", () => {
+    if(Mostrar.Verificar(codigo.value)==false){
+            let agp = new Array(codigo.value,nombre.value,desc.value,cantidad.value,coste.value,(Number(cantidad.value) * Number(coste.value)))
+            Mostrar.AgregarInicio(agp)
+            lista.innerHTML = agp[0]+" "+agp[1]+" "+agp[2]+" "+agp[3]+" "+agp[4]+" "+agp[5]+ "</br > Agregad0 con exito"
+        }else{
+            alert ("Codigo Repetido")
+        }
+});
+btneliminarprimero.addEventListener("click", () =>{
+    if(Mostrar.inicio){
+        let current = Mostrar.Recorrer(null)
+        lista.innerHTML = current.codigo+" "+current.nombre+" "+current.desc+" "+current.cantidad+" "+current.costo+" "+current.total+" "+ "</br >  Eliminado con exito!"
+        Mostrar.EliminarInicio()
+    }else{
+        alert("N0 se encontraron productos.")
+    }
 });
