44c31b5a19c35fc9a0cf7610c3583d73043ee215
Alber321 <lsolis2@ucol.mx>
2020-11-07 19:58:25 -0600

    Funciones nuevas Varias

--- main.js
+++ main.js
@@ -45,7 +45,7 @@ class Inventario{
         };
         this.size++
     };
-    ingresarP(codigo, nombre, desc, cantidad, coste, index){
+    PosEspecifico(codigo, nombre, desc, cantidad, coste, index){
         if(index<0 || index > this.size){
             return null
         };
@@ -65,5 +65,68 @@ class Inventario{
         };
         this.size++;
     };
-
+    AgregarInicio(codigo, nombre, desc, cantidad, coste){
+        const nuevop = new Producto(codigo, nombre, desc, cantidad, coste)
+        console.log();
+        let current = this.inicio;
+        nuevop.siguiente = current;
+        this.inicio = nuevop;
+    }
+    Recorrer(producto){
+        if(producto==null){
+            return this.inicio;
+        }else {
+            return producto.siguiente;
+        };
+    };
+    Verificar(codigo){
+      if(this.inicio == null){
+          return false;
+      };
+      let current = this.inicio;
+      while(current){
+          if(current.codigo === codigo){
+              return true;
+          };
+          current = current.siguiente;
+      };
+      return false;
+    };
+    BuscarCode(codigo){
+        if(this.inicio == null){
+            return null;
+        };
+        let current = this.inicio;
+        while(current){
+            if(current.codigo === codigo){
+                return current;
+            };
+        };
+        return null;
+    };
+    EliminarCode(codigo){
+        let current = this.inicio;
+        let previo = null;
+        while(current != null){
+            if(current.codigo == codigo){
+                if(!previo){
+                    this.inicio = current.siguiente;
+                }else{
+                    previo.siguiente = current.siguiente;
+                };
+                this.else--;
+                return current.codigo;
+            };
+            previo = current;
+            current = current.siguiente;
+        };
+        return null;
+    };
+    EliminarInicio(){
+        if(this.size>1){
+            Lista.index = Lista.inicio.siguiente;
+        };
+        this.size--;
+    };
+    
 };
